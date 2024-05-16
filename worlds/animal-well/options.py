8d8ca7c5ff1c0d5fe2fa30a1595001953586f945
Scipio Wright <scipiowright@gmail.com>
2024-05-16 10:39:12 -0400

    Add goals, add evil egg location

--- worlds/animal-well/options.py
+++ worlds/animal-well/options.py
@@ -3,6 +3,32 @@ from dataclasses import dataclass
 from Options import DefaultOnToggle, Toggle, StartInventoryPool, Choice, Range, TextChoice, PerGameCommonOptions
 
 
+class Goal(Choice):
+    """
+    What you need to do to beat the game.
+    Fireworks requires you to get the 4 flames and defeat the Manticore.
+    Bunny Land requires you to find the Evil Egg, bring it to the incubator, and leave the Well.
+    Egg Song requires you to collect the amount of eggs you need to play the Egg Song, then open the Evil Egg chest.
+    """
+    internal_name: "goal"
+    display_name: "Goal"
+    option_fireworks: 1
+    option_bunny_land: 2
+    option_egg_song: 3
+    default: 0
+
+
+class EvilEggLocation(Choice):
+    """
+    Choose whether the Evil Egg is shuffled into the multiworld item pool or placed in its vanilla location, requiring the Egg Song to access it.
+    """
+    internal_name: "evil_egg_location"
+    display_name: "Evil Egg Location"
+    option_randomized: 0
+    option_vanilla: 1
+    default: 1
+
+
 class EggsNeeded(Range):
     """
     How many Eggs you need to do the Egg Song.
@@ -46,7 +72,9 @@ class BubbleJumping(Toggle):
 @dataclass
 class AnimalWellOptions(PerGameCommonOptions):
     start_inventory_from_pool: StartInventoryPool
+    goal: Goal
     eggs_needed: EggsNeeded
+    evil_egg_location: EvilEggLocation
     bunnies_as_checks: BunniesAsChecks
     candle_checks: CandleChecks
     bubble_jumping: BubbleJumping
