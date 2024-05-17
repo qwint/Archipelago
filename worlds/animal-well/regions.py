4ceb2843746aa82a08504e62d3293077bc9ca510
Scipio Wright <scipiowright@gmail.com>
2024-05-17 10:44:37 -0400

    Update regions.py

--- worlds/animal-well/regions.py
+++ worlds/animal-well/regions.py
@@ -1,5 +1,6 @@
 from typing import Dict, List, Set, NamedTuple
 from enum import IntEnum
+import .item_names as iname
 
 
 # makes it more convenient to put together imo
@@ -13,39 +14,19 @@ class AWData(NamedTuple):
     rules: List[List[str]] = []  # how to access it
 
 
-# todo: move this to a constants file or something later?
-firecrackers = "Firecrackers"
-disc = "Disc"
-flute = "Flute"
-bubble = "Bubble Wand"
-bubble_short = "Bubble Wand - Short Range"  # small amount of bubble jumps, reasonable with just wand
-bubble_long = "BB Wand"  # large amount of jumps, reasonable with BB wand
-slink = "Slink"
-yoyo = "Yo-Yo"
-lantern = "Lantern"
-uv = "UV Lantern"
-top = "Top"
-wheel = "Wheel"
-remote = "Remote"
-ball = "Bouncy Ball"
-m_disc = "Mock Disc"
-e_medal = "E Medal"
-k_medal = "K Medal"
-s_medal = "S Medal"
-
-
 # instructions for contributors:
 # the outer string is the name of the origin region
 # the inner string is the name of the destination region or location
 # use AWData to specify if it's a region or location, and then put the rules in the second parameter if any
-# add item names used within rules to the floating variables above
+# add item names used within rules to the names.py file if any are missing
+# probably will change iname to something else later, but using it for now since find and replace exists
 # reason: we will probably change the names of things, so this'll make it easier
 # if you want to add something like an event to a rule, don't add it to the variables above, just write out the string in quotes
 # this is to set them apart from the rest for now, just making it easier as we write it initially
 traversal_requirements: Dict[str, Dict[str, AWData]] = {
     "Starting Area": {
         "Central Area":
-            AWData(AWType.region, [[firecrackers]),  # needed to get past ghost, not sure if randomizing this yet?
+            AWData(AWType.region, [[iname.firecrackers]),  # needed to get past ghost, not sure if randomizing this yet?
         "Candle Room":
             AWData(AWType.region, [["Light All Candles"]]),  # turn this into an event later
         "Ceiling Match":  # rename, this is the match that everyone apparently got last
