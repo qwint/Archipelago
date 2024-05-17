a57c6e50cc353e4331cd939ed2619e9041fb7400
Scipio Wright <scipiowright@gmail.com>
2024-05-17 15:30:37 -0400

    Update item_names.py

--- worlds/animal-well/item_names.py
+++ worlds/animal-well/item_names.py
@@ -1,19 +1,23 @@
+# actual items
 firecrackers = "Firecrackers"
 disc = "Disc"
-flute = "Flute"
-bubble = "Bubble Wand"
-bubble_short = "Bubble Wand - Short Range"  # small amount of bubble jumps, reasonable with just wand
-bubble_long = "BB Wand"  # large amount of jumps, reasonable with BB wand
+flute = "Animal Flute"
+bubble = "B. Wand"
+bubble_long = "B.B. Wand"  # large amount of jumps, reasonable with B.B. Wand
 slink = "Slink"
-yoyo = "Yo-Yo"
+yoyo = "Yoyo"
 lantern = "Lantern"
 uv = "UV Lantern"
 top = "Top"
 wheel = "Wheel"
 remote = "Remote"
-ball = "Bouncy Ball"
+ball = "B. Ball"
 m_disc = "Mock Disc"
-e_medal = "E Medal"
-k_medal = "K Medal"
-s_medal = "S Medal"
+fanny_pack = "F. Pack"
+e_medal = "E. Medal"
+k_medal = "K. Medal"
+s_medal = "S. Medal"
 match = "Match"
+
+# fake items, for the purposes of rules
+bubble_short = "Bubble Jumping - Short"
