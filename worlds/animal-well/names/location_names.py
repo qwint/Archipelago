863e701c5f6d3313c7e442109c8b3b7e299a9971
Scipio Wright <scipiowright@gmail.com>
2024-05-18 13:40:33 -0400

    Explore dog area a bit

--- worlds/animal-well/names/location_names.py
+++ worlds/animal-well/names/location_names.py
@@ -5,10 +5,17 @@ pencil_chest = "Pencil Chest"
 b_wand_chest = "B. Wand Chest"
 flute_chest = "Animal Flute Chest"
 
+disc_spot = "Disc Spot"  # rename
+
 ceiling_match_start = "Squirrel Ceiling Match"
 fish_mural_match = "Fish Mural Match"
+dog_match_switch = "Dog Switch Area Match"  # rename, in that spot where you throw the between the levers
+dog_match_upper_east = "Dog Match by Fish Pipe"
 
-first_candle = "Squirrel First Candle"
+first_candle = "Squirrel First Candle"  # the obvious first one
+dog_candle_dark = "Dog Dark Room Candle"  # the one in the dark room a few rooms after your first dog encounter
+dog_candle_switch_box = "Dog Candle in Switch Room with Box"  # definitely rename
+dog_candle_switch = "Dog Candle in Switch Room without Box"  # definitely rename
 
 key_chinchilla = "Chinchilla Key"  # early in the green area
 
@@ -27,3 +34,5 @@ disc_spike_bunny = "Disc Spike Bunny"  # rename
 water_spike_bunny = "Water Spike Bunny"  # rename
 barcode_bunny = "Paper Bunny"  # printer or barcode both get you it
 chinchilla_vine_bunny = "Chinchilla Bunny"  # the one where the code is covered by vines
+
+mama_cha = "Mama Cha"  # the same place as the barcode bunny
--- worlds/animal-well/regions.py
+++ worlds/animal-well/regions.py
@@ -21,12 +21,22 @@ s_disc_area = "Squirrel S. Disc Area"
 starting_after_ghost = "Squirrel After Ghost"
 fast_travel = "Fast Travel Room"
 bird_area = "Bird Area"  # the central portion of the map
+bird_capybara_waterfall = "Bird Capybara Waterfall"  # up and right of the ladder
 candle_area = "Squirrel Candle Area"
 fish_area = "Fish Main"
 fish_west = "Fish West"  # after the b. wand chest, rename
 fish_tube_room = "Fish Tube Room"  # rename?
 bear_area = "Bear Main"
 bear_chinchilla_song_room = "Bear Chinchilla Song Room"  # where the bunny is
+dog_area = "Dog Main"
+dog_chinchilla_skull = "Dog Chinchilla Skull Room"
+dog_at_mock_disc = "Dog at Mock Disc Chest"
+dog_upper = "Dog Area Upper"  # rename this variable and name
+dog_upper_past_lake = "Dog Area Upper Past Lake"
+dog_upper_above_switch_lines = "Dog Area Upper Above Switch Lines"  # rename, that spot where you go up the levels?
+dog_upper_above_switch_lines_to_upper_east = "Dog Area Upper Above Switch Lines to Upper East"  # where the button is
+dog_upper_east = "Dog Area Upper East"  # to the right of the area above the switch lines
+wave_room = "Wave Room"  # rename after figuring out which area this is
 
 # instructions for contributors:
 # the outer string is the name of the origin region
@@ -37,17 +47,17 @@ bear_chinchilla_song_room = "Bear Chinchilla Song Room"  # where the bunny is
 # if you want to add something like an event to a rule, do so, that's fine
 # this is to set them apart from the rest for now, just making it easier as we write it initially
 traversal_requirements: Dict[str, Dict[str, AWData]] = {
-    fast_travel: {
-        starting_area:
-            AWData(AWType.region, [[iname.flute]]),
-        fish_west:
-            AWData(AWType.region, [[iname.flute]]),
-    },
     bird_area: {
         fish_area:
             AWData(AWType.region),
         bear_area:
             AWData(AWType.region),
+        dog_area:
+            AWData(AWType.region),
+        dog_upper:
+            AWData(AWType.region),
+        bird_capybara_waterfall:
+            AWData(AWType.region, [[iname.disc], [iname.bubble_short]]),
         lname.stamp_chest:
             AWData(AWType.location),
         lname.flute_chest:
@@ -57,6 +67,11 @@ traversal_requirements: Dict[str, Dict[str, AWData]] = {
         lname.bunny_mural:
             AWData(AWType.location, [[iname.remote]]),
     },
+    bird_capybara_waterfall: {
+        "Sweet Egg":
+            AWData(AWType.location),
+    },
+
     starting_area: {
         starting_after_ghost:  # it would feel weird to call this the central area imo
             AWData(AWType.region, [[iname.firecrackers]]),  # not sure if randoing firecrackers yet
@@ -83,6 +98,7 @@ traversal_requirements: Dict[str, Dict[str, AWData]] = {
         lname.map_chest:
             AWData(AWType.location),
     },
+
     fish_area: {
         fish_west:
             AWData(AWType.region, [[iname.bubble]]),  # todo: check what else can be used for this connection
@@ -117,6 +133,7 @@ traversal_requirements: Dict[str, Dict[str, AWData]] = {
         "Magic Egg":  # open the gate in the fish tube room
             AWData(AWType.location),
     },
+
     bear_area: {
         lname.key_chinchilla:
             AWData(AWType.location),
@@ -129,6 +146,91 @@ traversal_requirements: Dict[str, Dict[str, AWData]] = {
         bear_area:
             AWData(AWType.region),
     },
+
+    dog_area: {
+        lname.disc_spot:
+            AWData(AWType.location),  # todo: figure out what to do for this
+        lname.dog_candle_dark:
+            AWData(AWType.location, [[iname.match]]),  # todo: figure out what we're doing for matches/candles
+        dog_chinchilla_skull:
+            AWData(AWType.region, [[iname.bubble]]),  # can get here without bubble jumps
+        "Upside Down Egg":  # upper right of switch platform room above second dog
+            AWData(AWType.location, [[iname.bubble_short]]),  # todo: there's another way here from above?
+        dog_at_mock_disc:  # you drop down to here, but can't get back up immediately
+            AWData(AWType.region),
+    },
+    dog_at_mock_disc: {
+        dog_area:  # can leave by hitting the button or jumping up to the ledge you came from
+            AWData(AWType.region, [[iname.bubble], [iname.disc]]),
+        "Sour Egg":  # when escaping the dachsund, jump up and right out of the tunnel
+            AWData(AWType.location),
+        bird_area:  # after the sour egg chest, you escape to the central area
+            AWData(AWType.region),
+    },
+    dog_chinchilla_skull: {
+        "Red Egg":
+            AWData(AWType.location, [[iname.firecrackers]])  # use a firecracker
+    },
+    dog_upper: {
+        dog_upper_past_lake:
+            AWData(AWType.region, [[iname.disc_hop], [iname.bubble_long]]),  # maybe not long?
+        dog_upper_above_switch_lines:
+            AWData(AWType.region, [[iname.disc], [iname.remote]]),  # double check you can use remote in here
+    },
+    dog_upper_past_lake: {
+        lname.dog_candle_switch_box:
+            AWData(AWType.location),
+        dog_upper_above_switch_lines:
+            AWData(AWType.region, [[iname.can_distract_dogs]]),  # need to get past the 3 dogs
+        lname.barcode_bunny:
+            AWData(AWType.location, [[iname.flute]]),  # add song req if we're shuffling songs
+        lname.mama_cha:
+            AWData(AWType.location, [[iname.flute]]),  # add song req if we're shuffling songs
+        # todo: floor is lava
+    },
+    dog_upper_above_switch_lines: {
+        lname.dog_match_switch:  # in the little switch area
+            AWData(AWType.location, [[iname.disc], [iname.remote]]),
+        lname.dog_candle_switch:
+            AWData(AWType.location, [[iname.match, iname.disc], [iname.match, iname.remote]]),
+        "Depraved Egg":  # in the little switch area, you need to take care of the ghost
+            AWData(AWType.location, [[iname.disc, iname.can_defeat_ghost], [iname.remote, iname.can_defeat_ghost]]),
+        dog_upper_above_switch_lines_to_upper_east:
+            AWData(AWType.region, [[iname.disc], [iname.remote]]),
+    },
+    dog_upper_above_switch_lines_to_upper_east: {
+        dog_upper_above_switch_lines:
+            AWData(AWType.region),  # hit button, walk into the hallway
+        dog_upper_east:
+            AWData(AWType.region),
+    },
+    dog_upper_east: {
+        dog_upper_above_switch_lines_to_upper_east:
+            AWData(AWType.region, [[iname.bubble_short]]),  # jump up to the switch
+        lname.dog_match_upper_east:
+            AWData(AWType.location),
+        dog_upper:  # hit the dynamite switch to get back to the bird area and upper dog
+            AWData(AWType.region),
+        wave_room:  # take the bubble pipe by the dynamite, this is the really long pipe
+            AWData(AWType.region, [[iname.bubble]]),
+        # todo: get slink, go east past the room that needs it for a puzzle
+    },
+
+    wave_room: {
+        "Jade Egg":  # do the puzzle
+            AWData(AWType.location),
+        # todo: to the worm room
+        bird_capybara_waterfall:  # take the very long pipe to the sweet egg room
+            AWData(AWType.region, [[iname.bubble]]),
+    },
+
+    fast_travel: {
+        starting_area:
+            AWData(AWType.region, [[iname.flute]]),
+        fish_west:
+            AWData(AWType.region, [[iname.flute]]),
+    },
+
     fast_travel_fake: {
         fast_travel:
             AWData(AWType.region),  # probably never randomizing fast travel song, so no rule
