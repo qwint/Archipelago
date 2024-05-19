51b626dfdc5d95952728b28cd5e5590ea3bf6fb8
Scipio Wright <scipiowright@gmail.com>
2024-05-19 14:23:34 -0400

    Mapping out a lot of the frog area

--- worlds/animal-well/names/item_names.py
+++ worlds/animal-well/names/item_names.py
@@ -22,14 +22,21 @@ match = "Match"
 
 blue_flame = "B. Flame"
 
+song_home = "Top of the Well Song"
 song_chinchilla = "Chinchilla Song"  # the warp to the chinchilla vine platform bunny
 
 # fake items, for the purposes of rules
 bubble_short = "Bubble Jumping - Short"
 can_break_spikes = "Can Break Spikes"
+can_break_spikes_below = "Can Break Spikes Below"  # can break spikes but without disc basically
 can_open_flame = "Can Open Flame"  # you can break this with the flute and other items, need to verify which
 disc_hop = "Disc Jumping"  # hopping on a disc in midair without it bouncing first
 disc_hop_hard = "Consecutive Disc Jumps"  # hopping on a disc multiple times, or after a bubble jump
 can_distract_dogs = "Can Distract Dogs"
 can_defeat_ghost = "Can Defeat Ghost"
+ball_tricky = "Ball Tricky"  # for hitting things with the ball that aren't obviously meant to be
+two_keys = "Two Keys"  # there's one spot that has two key doors adjacent to each other
+
+# event items
+activated_frog_fast_travel = "Activated Frog Fast Travel"
 
