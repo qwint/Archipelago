bd0b2830ca27c044305251fb414a7e33ae3ef080
Scipio Wright <scipiowright@gmail.com>
2024-05-19 17:36:11 -0400

    Frog area finished mapping for now

--- worlds/animal-well/names/location_names.py
+++ worlds/animal-well/names/location_names.py
@@ -16,7 +16,7 @@ bear_slink_match = "Match in Bear Slink Stair Puzzle Chest"
 match_above_egg_room = "Match Above Egg Room"  # the one to the right of the dog lower entrance
 center_well_match = "Match in Center Well Chest"  # the one high up in the shaft
 guard_room_match = "Match in Guard Room Chest"
-frog_autoplatform_match = "Match in Frog Autoplatform Room Chest"
+match_under_mouse_statue = "Match under Mouse Statue"  # east bird area, need yoyo to get in
 
 first_candle = "Squirrel First Candle Chest"  # the obvious first one
 dog_candle_dark = "Dog Dark Room Candle Chest"  # the one in the dark room a few rooms after your first dog encounter
@@ -31,6 +31,7 @@ key_frog_guard_room_west = "Key Chest in West Frog Guard Room"
 key_frog_guard_room_east = "Key Chest in East Frog Guard Room"
 
 blue_flame = "B. Flame"
+gray_flame = "G. Flame"
 
 bunny_mural = "Community Bunny"
 map_bunny = "Doodle Bunny"
