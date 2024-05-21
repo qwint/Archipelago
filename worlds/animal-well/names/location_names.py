8af36d87f7cba75d8d971319869e9b22f6735bbd
GameWyrm <78181444+GameWyrm@users.noreply.github.com>
2024-05-20 19:59:01 -0700

    should probably save these just in case

--- worlds/animal-well/names/location_names.py
+++ worlds/animal-well/names/location_names.py
@@ -1,13 +1,27 @@
-# location names that aren't eggs
+# major unique items
 map_chest = "Map Chest"
 stamp_chest = "Stamp Chest"
 pencil_chest = "Pencil Chest"
 b_wand_chest = "B. Wand Chest"
 flute_chest = "Animal Flute Chest"
+slink_chest = "Slink Chest"
 yoyo_chest = "Yoyo Chest"
+mock_disc_chest = "Mock Disc Chest"
+disc_spot = "Wolf Statue Holding Disc"  # rename
+b_ball_chest = "B. Ball Chest"
+top_chest = "Top Chest"
+firecracker_first = "Pick Up Firecrackers"
+key_house = "House Key"
+key_office = "Office Key"
+medal_k = "K. Medal Shard" # you need three to open the kangaroo door
+medal_s = "S. Medal"
+medal_e = "E. Medal"
 
-disc_spot = "Disc Spot"  # rename
+# minor unique items
+mama_cha = "Mama Cha Chest"  # the same place as the barcode bunny at grass bowl
+squirrel_acorn = "Steal Squirrel's Acorn"
 
+# match chests
 ceiling_match_start = "Match in Tutorial Chest"
 fish_mural_match = "Match in Fish Mural Room Chest"
 dog_match_switch = "Match in Switch-Bounce Room Chest"  # rename, in that spot where you throw the between the levers
@@ -18,6 +32,7 @@ center_well_match = "Match in Center Well Chest"  # the one high up in the shaft
 guard_room_match = "Match in Guard Room Chest"
 match_under_mouse_statue = "Match under Mouse Statue"  # east bird area, need yoyo to get in
 
+# candle checks
 first_candle = "Squirrel First Candle"  # the obvious first one
 dog_candle_dark = "Dog Dark Room Candle"  # the one in the dark room a few rooms after your first dog encounter
 dog_candle_switch_box = "Dog Boxed Candle"
@@ -28,32 +43,72 @@ fish_candle = "Fish Candle in Penguin Room"
 frog_candle = "Frog Candle Switch Carousel"  # to screens to the right of the wombat save point
 bear_candle = "Bear Candle in Dark Maze"
 
-key_chinchilla = "Chinchilla Key"  # early in the green area
+# key chests
+key_bear_lower = "Key Chest in Lower Bear"  # early in the green area
+key_bear_upper = "Key Chest in Upper Bear" # get the chest to land on the chinchilla, maybe rename these two
 key_chest_mouse_head_lever = "Key Chest by Mouse Head Hitting Lever"  # rename definitely
 key_frog_guard_room_west = "Key Chest in West Frog Guard Room"
 key_frog_guard_room_east = "Key Chest in East Frog Guard Room"
+key_dog = "Key Chest in Dog with Chinchilla Crank" # maybe rename
 
-blue_flame = "B. Flame"
-gray_flame = "G. Flame"
+# flames
+flame_blue = "B. Flame" # fish area
+flame_green = "G. Flame" # frog area
+flame_pink = "P. Flame" # dog area
+flame_violet = "V. Flame" # bear area
 
+# eggs, not in any particular order
+egg_forbidden = "Forbidden Egg" # swordfish lake
+egg_vanity = "Vanity Egg" # kangaroo breakout
+egg_reference = "Reference Egg" # dog region disc with fans puzzle
+egg_brown = "Brown Egg" # next to Reference Egg
+egg_service = "Egg As A Service" # behind Bat room
+egg_upside_down = "Upside Down Egg" # dog many switches room
+egg_red = "Red Egg" # dog double chinchilla puzzle
+egg_friendship = "Friendship Egg" # dark room from pipe maze
+egg_plant = "Plant Egg" # two boxes with disc puzzle
+egg_future = "Future Egg" # dark room behind chinchilla vines
+egg_raw = "Raw Egg" # dog slinky box puzzle
+egg_evil = "Evil Egg" # turtle flute pool
+egg_orange = "Orange Egg" # dog egg under telephone
+egg_depraved = "Depraved Egg" # dog switch maze
+egg_sour = "Sour Egg" # daschund tunnels
+egg_sweet = "Sweet Egg" # above dog wheel
+egg_big = "Big Egg" # near Mock Disc Shrine
+egg_pickled = "Pickled Egg" # hidden spot in well wall
+egg_chocolate = "Chocolate Egg" # behind well wall switch blocks
+egg_post_modern = "Post Modern Egg" # well wall behind top dirt
+egg_truth = "Truth Egg" # bear above bottom left crow
+egg_transcendental = "Transcendental Egg" # below slink chest
+egg_swan = "Swan Egg" # bear next to Chameleon final boss entrance
+egg_shadow = "Shadow Egg" # near above arrow lift bridge
+egg_chaos = "Chaos Egg" # monke room
+egg_value = "Value Egg" # bear top hedgehog buttons
+egg_zen = "Zen Egg" # bear next to upper key
+egg_razzle = "Razzle Egg" # behind lowest chameleon
+egg_lf = "Laissez-faire Egg" # bear spook chinchilla room. Internal name may be bad but the full name is worse.
+egg_universal = "Universal Basic Egg" # next to Zen Egg
+egg_rain = "Rain Egg" # top dirt outside egg house
+egg_holiday = "Holiday Egg" # alcove outside egg house
+egg_65 = "65th Egg" # move to Major Items if Eggsanity becomes a setting
+
+# bnuuy
 bunny_mural = "Community Bunny"
-map_bunny = "Doodle Bunny"
-uv_bunny = "Invisible Bunny"
-fish_bunny = "Fish Bunny"  # rename?
-face_bunny = "Evil Bunny"  # rename?
-crow_bunny = "Singing Bunny"
-duckarabbit = "Green Bunny"  # rename?
-dream_bunny = "Imaginary Bunny"
-lava_bunny = "Lava Bunny"  # floor is lava
-tv_bunny = "Flashing Bunny"  # rename?
-ghost_dog_bunny = "Ghost Bunny"  # rename
-disc_spike_bunny = "Disc Spike Bunny"  # rename
-water_spike_bunny = "Water Spike Bunny"  # rename
-barcode_bunny = "Paper Bunny"  # printer or barcode both get you it
-chinchilla_vine_bunny = "Chinchilla Bunny"  # the one where the code is covered by vines
-
-mama_cha = "Mama Cha Chest"  # the same place as the barcode bunny
-
+bunny_map = "Doodle Bunny"
+bunny_uv = "Invisible Bunny"
+bunny_fish = "Fish Bunny"  # rename?
+bunny_face = "Evil Bunny"  # rename?
+bunny_crow = "Singing Bunny"
+bunny_duck = "Green Bunny"  # rename?
+bunny_dream = "Imaginary Bunny"
+bunny_lava = "Lava Bunny"  # floor is lava
+bunny_tv = "Flashing Bunny"  # rename?
+bunny_ghost_dog = "Ghost Bunny"  # rename
+bunny_disc_spike = "Disc Spike Bunny"  # rename
+bunny_water_spike = "Water Spike Bunny"  # rename
+bunny_barcode = "Paper Bunny"  # printer or barcode both get you it
+bunny_chinchilla_vine = "Chinchilla Bunny"  # the one where the code is covered by vines
+bunny_file_bud = "Flowering Bunny" # bunny from file start codes
 
 # event locations
 activate_frog_fast_travel = "Activate Frog Fast Travel"
