5184381a6ca90ceb7b517d7807fd8a6197c6f14e
Scipio Wright <scipiowright@gmail.com>
2024-05-20 20:24:03 -0400

    Clean out tunc stuff

--- worlds/animal-well/options.py
+++ worlds/animal-well/options.py
@@ -10,8 +10,8 @@ class Goal(Choice):
     Bunny Land requires you to find the 65th Egg, bring it to the incubator, and leave the Well.
     Egg Hunt requires you to collect the amount of eggs you need to open the 4th Egg Door, then open the chest inside.
     """
-    internal_name: "goal"
-    display_name: "Goal"
+    internal_name = "goal"
+    display_name = "Goal"
     option_fireworks: 1
     option_bunny_land: 2
     option_egg_hunt: 3
@@ -22,8 +22,8 @@ class EvilEggLocation(Choice):
     """
     Choose whether the 65th Egg is shuffled into the multiworld item pool or placed in its vanilla location, requiring opening the 4th Egg Door to access it.
     """
-    internal_name: "65th_egg_location"
-    display_name: "65th Egg Location"
+    internal_name = "65th_egg_location"
+    display_name = "65th Egg Location"
     option_randomized: 0
     option_vanilla: 1
     default: 1
