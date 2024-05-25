339bb0d28e44658adc40a501a5b404f1521b5e1d
Rooby-Roo <thegreenrobby@gmail.com>
2024-05-24 20:07:25 -0600

    reorder item_names to match location_names
    only after putting in all this labor did I notice you asked for items.py and location.py i cry

--- worlds/animal-well/names/item_names.py
+++ worlds/animal-well/names/item_names.py
@@ -1,69 +1,38 @@
 # this file is a reference to the items that will end up going in rule names
-# actual items
-firecrackers = "Firecrackers"
-disc = "Disc"
-flute = "Animal Flute"
+# major unique items
 bubble = "B. Wand"
-bubble_long = "B.B. Wand"  # large amount of jumps, reasonable with B.B. Wand
+flute = "Animal Flute"
 slink = "Slink"
 yoyo = "Yoyo"
-lantern = "Lantern"
-uv = "UV Lantern"
-top = "Top"
-wheel = "Wheel"
-remote = "Remote"
-ball = "B. Ball"
 m_disc = "Mock Disc"
+disc = "Disc"
+lantern = "Lantern"
+ball = "B. Ball"
+remote = "Remote"
+uv = "UV Lantern"
+wheel = "Wheel"
+top = "Top"
+bubble_long = "B.B. Wand"  # large amount of jumps, reasonable with B.B. Wand
+firecrackers = "Firecrackers"
+house_key = "House Key"
+office_key = "Office Key"
 fanny_pack = "F. Pack"
-e_medal = "E. Medal"
 k_shard = "K. Shard"
 k_medal = "K. Medal"  # a fake item. 3 K. Shards exist in pool
 s_medal = "S. Medal"
-match = "Match"
-key = "Key"
-house_key = "House Key"
-office_key = "Office Key"
+e_medal = "E. Medal"
 
+match = "Match"
+matchbox = "Matchbox"  # potentially an item if we want to simplify logic
+
+key = "Key"
+key_ring = "Key Ring"  # potentially an item if we want to simplify logic
+
+# flames
 blue_flame = "B. Flame"  # seahorse
-violet_flame = "V. Flame"  # chameleon
 green_flame = "G. Flame"  # ostritch
 pink_flame = "P. Flame"  # ghost dog
-
-song_home = "Top of the Well Song"
-song_chinchilla = "Chinchilla Song"  # the warp to the chinchilla vine platform bunny
-song_bobcat = "Bobcat Song"  # idk what we should do with this, but it kinda sucks as it is
-song_fish = "Skeleton Fish Song"  # teleports you to the right side of the lower screen of the fast travel room
-song_barcode = "Barcode Song"  # for barcode bunny
-
-# event items
-activated_bird_fast_travel = "Activated Bird Fast Travel"
-activated_bear_fast_travel = "Activated Bear Fast Travel"
-activated_frog_fast_travel = "Activated Frog Fast Travel"
-activated_squirrel_fast_travel = "Activated Squirrel Fast Travel"
-activated_fish_fast_travel = "Activated Fish Fast Travel"
-activated_dog_fast_travel = "Activated Dog Fast Travel"
-activated_hippo_fast_travel = "Activated Hippo Fast Travel"
-activated_bonefish_fast_travel = "Activated Bone Fish Fast Travel"
-event_candle_penguin_lit = "Lit the Candle by the Penguins"
-defeated_chameleon = "Defeated Chameleon"
-switch_for_post_modern_egg = "Activated Switch for Post Modern Egg"
-switch_next_to_bat_room = "Activated Switch next to Bat Room"  # for getting up to the fast travel spot in dog area
-light_all_candles = "Light all Candles"
-dog_wheel_flip = "Flipped Dog Wheel"
-
-# fake items, for the purposes of rules
-bubble_short = "Bubble Jumping - Short"
-can_break_spikes = "Can Break Spikes"
-can_break_spikes_below = "Can Break Spikes Below"  # can break spikes but without disc basically
-can_open_flame = "Can Open Flame"  # you can break this with the flute and other items, need to verify which
-disc_hop = "Disc Jumping"  # hopping on a disc in midair without it bouncing first
-disc_hop_hard = "Consecutive Disc Jumps"  # hopping on a disc multiple times, or after a bubble jump
-can_distract_dogs = "Can Distract Dogs"
-can_defeat_ghost = "Can Defeat Ghost"
-ball_tricky = "Ball Tricky"  # for hitting things with the ball that aren't obviously meant to be
-two_keys = "Two Keys"  # there's one spot that has two key doors adjacent to each other
-tanking_damage = "Tanking Damage"  # for spots you can get to by taking up to 3 hearts of damage
-weird_skips = "Weird Skips"  # skips that are questionably in logic and may or may not constitute a difficulty setting later
+violet_flame = "V. Flame"  # chameleon
 
 # eggs, in a particular order but not one that actually matters
 egg_forbidden = "Forbidden Egg"  
@@ -130,4 +99,42 @@ egg_desert = "Desert Egg"
 egg_scarlet = "Scarlet Egg"
 egg_obsidian = "Obsidian Egg"
 egg_golden = "Golden Egg"
+
 egg_65 = "65th Egg"
+
+# event items
+activated_bird_fast_travel = "Activated Bird Fast Travel"
+activated_bear_fast_travel = "Activated Bear Fast Travel"
+activated_frog_fast_travel = "Activated Frog Fast Travel"
+activated_squirrel_fast_travel = "Activated Squirrel Fast Travel"
+activated_fish_fast_travel = "Activated Fish Fast Travel"
+activated_dog_fast_travel = "Activated Dog Fast Travel"
+activated_hippo_fast_travel = "Activated Hippo Fast Travel"
+activated_bonefish_fast_travel = "Activated Bone Fish Fast Travel"
+event_candle_penguin_lit = "Lit the Candle by the Penguins"
+defeated_chameleon = "Defeated Chameleon"
+switch_for_post_modern_egg = "Activated Switch for Post Modern Egg"
+switch_next_to_bat_room = "Activated Switch next to Bat Room"  # for getting up to the fast travel spot in dog area
+light_all_candles = "All Candles Lit"
+dog_wheel_flip = "Flipped Dog Wheel"
+
+# fake items, for the purposes of rules
+bubble_short = "Bubble Jumping - Short"
+can_break_spikes = "Can Break Spikes"
+can_break_spikes_below = "Can Break Spikes Below"  # can break spikes but without disc basically
+can_open_flame = "Can Open Flame"  # you can break this with the flute and other items, need to verify which
+disc_hop = "Disc Jumping"  # hopping on a disc in midair without it bouncing first
+disc_hop_hard = "Consecutive Disc Jumps"  # hopping on a disc multiple times, or after a bubble jump
+can_distract_dogs = "Can Distract Dogs"
+can_defeat_ghost = "Can Defeat Ghost"
+ball_tricky = "Ball Tricky"  # for hitting things with the ball that aren't obviously meant to be
+two_keys = "Two Keys"  # there's one spot that has two key doors adjacent to each other
+tanking_damage = "Tanking Damage"  # for spots you can get to by taking up to 3 hearts of damage
+weird_skips = "Weird Skips"  # skips that are questionably in logic and may or may not constitute a difficulty setting later
+
+# songs, to potentially be randomized
+song_home = "Top of the Well Song"
+song_chinchilla = "Chinchilla Song"  # the warp to the chinchilla vine platform bunny
+song_bobcat = "Bobcat Song"  # idk what we should do with this, but it kinda sucks as it is
+song_fish = "Skeleton Fish Song"  # teleports you to the right side of the lower screen of the fast travel room
+song_barcode = "Barcode Song"  # for barcode bunny
