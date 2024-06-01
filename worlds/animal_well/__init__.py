6086399ea627e94322eba3bc84c2a04e3de3255b
Scipio Wright <scipiowright@gmail.com>
2024-06-01 19:19:08 -0400

    Clean up more bugs

--- worlds/animal_well/__init__.py
+++ worlds/animal_well/__init__.py
@@ -69,6 +69,7 @@ class AnimalWellWorld(World):
         # if these options conflict, override -- player is warned in the option description
         if not self.options.bunny_warps_in_logic and self.options.bunnies_as_checks:
             self.options.bunny_warps_in_logic.value = True
+
         # Universal tracker stuff, shouldn't do anything in standard gen
         if hasattr(self.multiworld, "re_gen_passthrough"):
             if "ANIMAL WELL" in self.multiworld.re_gen_passthrough:
