f29598e8b20feccf661efb8038d80d1351f476f8
Scipio Wright <scipiowright@gmail.com>
2024-06-02 00:01:26 -0400

    Make test work

--- worlds/animal_well/test/test_access.py
+++ worlds/animal_well/test/test_access.py
@@ -1,12 +1,19 @@
-from . import AWTestBase
+
 from .. import options
 from ..names import ItemNames as iname, LocationNames as lname
+
+from test.bases import WorldTestBase
+
+
+class AWTestBase(WorldTestBase):
+    game = "ANIMAL WELL"
+    player = 1
 
 
 class TestAccess(AWTestBase):
     # test that you can't get to the B. Ball chest until you have the K. Medal or the Ball
     def test_bball_access(self) -> None:
-        self.collect_all_but([iname.ball, iname.k_shard])
+        self.collect_all_but([iname.ball, iname.k_shard, iname.k_medal])  # why does this collect events???
         self.assertFalse(self.can_reach_location(lname.b_ball_chest))
         self.collect_by_name([iname.k_shard])
         self.assertTrue(self.can_reach_location(lname.b_ball_chest))
