0a636585be400bf448a4f8ffea15622eb6ed3de1
Scipio Wright <scipiowright@gmail.com>
2024-06-02 12:37:32 -0400

    Make tests work

--- worlds/animal_well/test/test_access.py
+++ worlds/animal_well/test/test_access.py
@@ -1,13 +1,13 @@
 
-from .. import options
+from . import AWTestBase
 from ..names import ItemNames as iname, LocationNames as lname
 
 from test.bases import WorldTestBase
 
 
-class AWTestBase(WorldTestBase):
-    game = "ANIMAL WELL"
-    player = 1
+# class AWTestBase(WorldTestBase):
+#     game = "ANIMAL WELL"
+#     player = 1
 
 
 class TestAccess(AWTestBase):
@@ -17,3 +17,16 @@ class TestAccess(AWTestBase):
         self.assertFalse(self.can_reach_location(lname.b_ball_chest))
         self.collect_by_name([iname.k_shard])
         self.assertTrue(self.can_reach_location(lname.b_ball_chest))
+
+
+class TestBunnyAccess(AWTestBase):
+    options = {
+        "bunnies_as_checks": "all_bunnies"
+    }
+
+    # test that the B.B. Wand event functions properly
+    def test_bubble_long(self) -> None:
+        self.collect_all_but([iname.bubble, iname.bubble_long]),
+        self.assertFalse(self.can_reach_location(lname.bunny_water_spike))
+        self.collect_by_name([iname.bubble]),
+        self.assertTrue(self.can_reach_location(lname.bunny_water_spike))
