7d6247f4d735b24b3a1eef2537302118b7f31722
Scipio Wright <scipiowright@gmail.com>
2024-06-18 11:01:13 -0400

    More logic updates

--- worlds/animal_well/region_data.py
+++ worlds/animal_well/region_data.py
@@ -228,7 +228,7 @@ traversal_requirements: Dict[Union[lname, rname], Dict[Union[lname, rname], AWDa
         rname.fish_west:
             AWData(AWType.region, [[iname.bubble]]),  # fish pipe left of the save point
         rname.fish_boss_1:  # disc is required to solve both the windbox puzzle and to cross the whale room
-            AWData(AWType.region, [[iname.disc]]),
+            AWData(AWType.region, [[iname.disc], [iname.weird_skips]]),  # position on ladder just right
         rname.bobcat_room:
             AWData(AWType.region, [[iname.top]]), 
         lname.candle_fish:  # spike breaking presumed by access
