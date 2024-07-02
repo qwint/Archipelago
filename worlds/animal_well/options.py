394aa1905f3a01fbb4c33770fb79f6dc58d3e9e9
Scipio Wright <scipiowright@gmail.com>
2024-07-02 09:16:08 -0400

    Uncomment option groups

--- worlds/animal_well/options.py
+++ worlds/animal_well/options.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from typing import Dict, Any
-from Options import DefaultOnToggle, Toggle, StartInventoryPool, Choice, Range, PerGameCommonOptions  # , OptionGroup
+from Options import DefaultOnToggle, Toggle, StartInventoryPool, Choice, Range, PerGameCommonOptions, OptionGroup
 
 
 class Goal(Choice):
@@ -164,14 +164,14 @@ class AnimalWellOptions(PerGameCommonOptions):
     exclude_song_chests: ExcludeSongChests
 
 
-# aw_option_groups = [
-#     OptionGroup("Logic Options", [
-#         BubbleJumping,
-#         DiscHopping,
-#         WheelHopping,
-#         WeirdTricks,
-#     ])
-# ]
+aw_option_groups = [
+    OptionGroup("Logic Options", [
+        BubbleJumping,
+        DiscHopping,
+        WheelHopping,
+        WeirdTricks,
+    ])
+]
 
 aw_option_presets: Dict[str, Dict[str, Any]] = {
     "Animal Hell": {
