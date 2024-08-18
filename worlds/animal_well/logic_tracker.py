5c15e0477bcdf965a61660c182535c2b12dd0db9
Scipio Wright <scipiowright@gmail.com>
2024-08-17 22:40:57 -0400

    Adjust tracker to actually properly check out of logic locations

--- worlds/animal_well/logic_tracker.py
+++ worlds/animal_well/logic_tracker.py
@@ -36,6 +36,8 @@ class AnimalWellTracker:
 
     # the player's current inventory, including event items and the 65th egg, excluding eggs
     full_inventory: Set[str] = set()
+    # same as above, but includes out of logic inventory too
+    out_of_logic_full_inventory: Set[str] = set()
     # update these manually
     # egg_tracker is a set instead of an int to properly deal with duplicates from get_item, start inventory, etc.
     egg_tracker: Set[str] = set()
@@ -51,23 +53,24 @@ class AnimalWellTracker:
     # set in_logic to True for regions_in_logic, False for regions_out_of_logic
     def update_spots_status(self, in_logic: bool) -> None:
         regions_set = self.regions_in_logic if in_logic else self.regions_out_of_logic
-        region_count = len(regions_set)
-        inventory_count = len(self.full_inventory)
+        inventory_set = self.full_inventory if in_logic else self.out_of_logic_full_inventory
+        region_and_item_inventory = regions_set.union(inventory_set)
+        inventory_count = len(region_and_item_inventory)
         for origin, destinations in traversal_requirements.items():
-            if origin not in regions_set:
+            if origin not in region_and_item_inventory:
                 continue
             for destination_name, destination_data in destinations.items():
                 if destination_data.type == AWType.region:
-                    if destination_name in regions_set:
+                    if destination_name in region_and_item_inventory:
                         continue
                     # if it's a bunny warp, bunny warps in logic is off, and we're updating the in logic regions
                     if (destination_data.bunny_warp and in_logic
                             and not self.player_options[BunnyWarpsInLogic.internal_name]):
                         continue
-                # bools are ints
                 if destination_data.type == AWType.location:
                     if destination_data.event:
                         self.check_logic_status.setdefault(str(destination_name), 0)
+                    # bools are ints
                     if self.check_logic_status[destination_name] >= 1 + in_logic:
                         continue
                     # skip bunnies that aren't included in the location pool
@@ -76,6 +79,9 @@ class AnimalWellTracker:
                              or (self.player_options[BunniesAsChecks.internal_name] == BunniesAsChecks.option_exclude_tedious
                                  and destination_name in [lname.bunny_mural, lname.bunny_dream,
                                                           lname.bunny_uv, lname.bunny_lava]))):
+                        self.check_logic_status[destination_name] = CheckStatus.checked
+                    # we ignore these and rely on the event version
+                    elif destination_data.loc_type == LocType.candle:
                         continue
 
                 met: bool = False
@@ -84,7 +90,7 @@ class AnimalWellTracker:
                     if len(req_list) == 0:
                         met = True
                         break
-                    if set(req_list).issubset(self.full_inventory):
+                    if set(req_list).issubset(region_and_item_inventory):
                         met = True
                         break
 
@@ -96,38 +102,40 @@ class AnimalWellTracker:
                         regions_set.add(destination_name)
                     elif destination_data.type == AWType.location:
                         self.check_logic_status[destination_name] = CheckStatus.out_of_logic + in_logic
-                        if destination_data.event:
-                            self.full_inventory.add(destination_data.event)
+                        if destination_data.event and "Candle" not in destination_name:
+                            inventory_set.add(destination_data.event)
                             self.check_logic_status[destination_name] = CheckStatus.checked
         # if the length of the region set or inventory changed, loop through again
-        if region_count != len(regions_set) or inventory_count != len(self.full_inventory):
+        if inventory_count != len(regions_set) + len(inventory_set):
             self.update_spots_status(in_logic)
-
-    def update_inventory_with_events(self) -> None:
-        for origin, destinations in traversal_requirements.items():
-            for destination_name, destination_data in destinations.items():
-                if destination_data.event and self.check_logic_status.get(destination_name, 0) == CheckStatus.checked:
-                    self.full_inventory.add(destination_data.event)
 
     def put_logic_items_in_inventory(self) -> None:
         # hacky but whatever
         if self.upgraded_b_wand:
             self.full_inventory.add(iname.bubble_long_real)
+            self.out_of_logic_full_inventory.add(iname.bubble_long_real)
         if self.key_count >= 6:
             self.full_inventory.add(iname.key_ring)
+            self.out_of_logic_full_inventory.add(iname.key_ring)
         if self.match_count >= 9:
             self.full_inventory.add(iname.matchbox)
+            self.out_of_logic_full_inventory.add(iname.matchbox)
 
         if iname.bubble_long_real in self.full_inventory:
             self.full_inventory.add(iname.bubble_short)
             self.full_inventory.add(iname.bubble_long)
-        elif iname.bubble in self.full_inventory:
+        if iname.bubble in self.full_inventory:
+            self.out_of_logic_full_inventory.add(iname.bubble_short)
+            self.out_of_logic_full_inventory.add(iname.bubble_long)
             if self.player_options[BubbleJumping.internal_name] >= BubbleJumping.option_exclude_long_chains:
                 self.full_inventory.add(iname.bubble_short)
             if self.player_options[BubbleJumping.internal_name] >= BubbleJumping.option_on:
                 self.full_inventory.add(iname.bubble_long)
 
         if iname.wheel in self.full_inventory:
+            self.out_of_logic_full_inventory.add(iname.wheel_hop)
+            self.out_of_logic_full_inventory.add(iname.wheel_climb)
+            self.out_of_logic_full_inventory.add(iname.wheel_hard)
             if self.player_options[WheelHopping.internal_name] >= WheelHopping.option_simple:
                 self.full_inventory.add(iname.wheel_hop)
                 self.full_inventory.add(iname.wheel_climb)
@@ -137,13 +145,18 @@ class AnimalWellTracker:
         # this is temporary -- remove if we detect when the player has traded the mock disc for the real disc
         if iname.m_disc in self.full_inventory:
             self.full_inventory.add(iname.disc)
+            self.out_of_logic_full_inventory.add(iname.disc)
 
         if iname.disc in self.full_inventory:
+            self.out_of_logic_full_inventory.add(iname.disc_hop)
+            self.out_of_logic_full_inventory.add(iname.disc_hop_hard)
             if self.player_options[DiscHopping.internal_name] >= DiscHopping.option_single:
                 self.full_inventory.add(iname.disc_hop)
             if self.player_options[DiscHopping.internal_name] >= DiscHopping.option_multiple:
                 self.full_inventory.add(iname.disc_hop_hard)
 
+        self.out_of_logic_full_inventory.add(iname.weird_tricks)
+        self.out_of_logic_full_inventory.add(iname.tanking_damage)
         if self.player_options[WeirdTricks.internal_name]:
             self.full_inventory.add(iname.weird_tricks)
             self.full_inventory.add(iname.tanking_damage)
@@ -151,12 +164,18 @@ class AnimalWellTracker:
         for helper_name, items in helper_reference.items():
             for item in items:
                 if item in self.full_inventory:
+                    # need to change this later if the helpers end up having logic tricks too
+                    self.out_of_logic_full_inventory.add(helper_name)
                     self.full_inventory.add(helper_name)
                     break
 
     def update_checks_and_regions(self) -> None:
         self.put_logic_items_in_inventory()
-        self.update_inventory_with_events()
         self.update_spots_status(in_logic=True)
         self.update_spots_status(in_logic=False)
-        print(self.check_logic_status)
+
+    def clear_inventories(self) -> None:
+        self.full_inventory.clear()
+        self.out_of_logic_full_inventory.clear()
+        self.regions_in_logic = {rname.starting_area}
+        self.regions_out_of_logic = {rname.starting_area}
