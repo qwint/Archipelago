d6d4444943fb58a0829a95e0245d4912a63275c4
qwint <qwint.42@gmail.com>
2025-01-19 14:27:47 -0600

    clean up monkeypatch, add more filters and commands to toggle them

--- worlds/fancy_client/fancy_client.py
+++ worlds/fancy_client/fancy_client.py
@@ -1,4 +1,4 @@
-from CommonClient import get_base_parser, server_loop, gui_enabled
+from CommonClient import get_base_parser, server_loop, gui_enabled, logger
 import asyncio
 
 tracker_loaded = False
@@ -9,13 +9,95 @@ except ModuleNotFoundError:
     from CommonClient import CommonContext as SuperContext
 
 
+class FancyCommandProcessor(SuperContext.command_processor):
+    def _cmd_prog(self, value=""):
+        """Toggles if Only Prog are shown in the client. Can set to true/false or be toggled without a value."""
+        if value.lower() in ("true", "false",):
+            target = value.lower() == "true"
+        else:
+            target = not self.ctx.prog_only
+        self.ctx.prog_only = target
+        logger.info(f"Prog Only set to {target}")
+
+    def _cmd_me(self, value=""):
+        """Toggles if Only Messages related to the current slot are shown in the client. Can set to true/false or be toggled without a value."""
+        if value.lower() in ("true", "false",):
+            target = value.lower() == "true"
+        else:
+            target = not self.ctx.only_me
+        self.ctx.only_me = target
+        logger.info(f"Only Me set to {target}")
+
+    def _cmd_watch_players(self, *names):
+        """
+        Takes a list of player names to filter by, not compatible with Only Me and will be ignored when Only Me is set.
+        When used without a name it will clear the list and disable the function.
+        """
+        if not self.ctx.slot_info:
+            logger.info("Please set watch players after connecting.")
+            return
+        if not names:
+            logger.info("Watch Players filter cleared.")
+            self.ctx.filter_for = set()
+            return
+        slot_ids = {slot_id for slot_id, slot in self.ctx.slot_info.items() if slot.name in names}
+        group_ids = {slot_id for slot_id, slot in self.ctx.slot_info.items()
+                     if any(s in slot.group_members for s in slot_ids)}
+        self.ctx.filter_for = slot_ids | group_ids
+        if self.ctx.filter_for:
+            logger.info("Watching Players list updated for slots"
+                        f"{self.ctx.slot_info[slot].name for slot in self.ctx.filter_for}")
+        else:
+            logger.info("No matching slots found for those player names, Watch Players filter cleared.")
+
+
 def run_fancy_textclient():
     class TextContext(SuperContext):
         # Text Mode to use !hint and such with games that have no text entry
         tags = SuperContext.tags | {"TextOnly"}
         game = ""  # empty matches any game since 0.3.2
+        command_processor = FancyCommandProcessor
         items_handling = 0b111  # receive all items for /received
         want_slot_data = False  # Can't use game specific slot_data
+
+        prog_only = False
+        only_me = False
+        filter_for = set()
+
+        def make_gui(self):
+            ui = super().make_gui()
+
+            class CCApp(ui):
+                def print_json(self, data):
+                    text = self.json_to_kivy_parser(data)
+                    # this is called up here because some parser dominos I couldn't follow
+                    # need the ref count set on __call__ properly even if the text is not displayed
+
+                    if self.ctx.prog_only:
+                        flags = [part["flags"] for part in data if "flags" in part]
+                        if flags and not all(flag & 0b001 for flag in flags):
+                            # if we're looking at an itemsend that is not prog don't print anything
+                            return
+                    if self.ctx.only_me:
+                        players = [part["player"] for part in data if "player" in part]
+                        if any(self.ctx.slot_concerns_self(slot) for slot in players):
+                            pass
+                        else:
+                            return
+                    elif self.ctx.filter_for:  # ignored when only me is set
+                        players = [part["player"] for part in data if "player" in part]
+                        if not players or any(player in self.ctx.filter_for for player in players):
+                            pass
+                        else:
+                            print(data)
+                            return
+
+                    self.log_panels["Archipelago"].on_message_markup(text)
+                    self.log_panels["All"].on_message_markup(text)
+
+            return CCApp
+
+# from here down is generic text client stuff
 
         async def server_auth(self, password_requested: bool = False):
             if password_requested and not self.password:
@@ -40,15 +122,6 @@ def run_fancy_textclient():
             ctx.run_generator()
         if gui_enabled:
             ctx.run_gui()
-            old_print_json = ctx.ui.print_json
-
-            def new_print_json(data):
-                flags = [part["flags"] for part in data if "flags" in part]
-                if flags and not all(flag & 0b001 for flag in flags):
-                    # if we're looking at an itemsend that is not prog don't print anything
-                    return
-                old_print_json(data)
-            ctx.ui.print_json = new_print_json
         ctx.run_cli()
 
         await ctx.exit_event.wait()
