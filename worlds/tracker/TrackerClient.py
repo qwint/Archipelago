9a96c70bd917729f3acea309fcf476d322e476fa
qwint <qwint.42@gmail.com>
2025-06-01 10:21:21 -0500

    a bonus fix for your troubles, thanks test actions

--- worlds/tracker/TrackerClient.py
+++ worlds/tracker/TrackerClient.py
@@ -608,8 +608,8 @@ class TrackerGameContext(CommonContext):
             tracker_divider = MDDivider(size_hint_y=None, height=dp(1))
             self.tracker_total_locs_label = MDLabel(text="Locations: 0/0", halign="center")
             self.tracker_logic_locs_label = MDLabel(text="In Logic: 0", halign="center")
-            self.tracker_glitched_locs_label = MDLabel(text=f"Glitched: [color={get_ut_color("glitched")}]0[/color]",  halign="center")
-            self.tracker_hinted_locs_label = MDLabel(text=f"Hinted: [color={get_ut_color("hinted_in_logic")}]0[/color]", halign="center")
+            self.tracker_glitched_locs_label = MDLabel(text=f"Glitched: [color={get_ut_color('glitched')}]0[/color]",  halign="center")
+            self.tracker_hinted_locs_label = MDLabel(text=f"Hinted: [color={get_ut_color('hinted_in_logic')}]0[/color]", halign="center")
             self.tracker_glitched_locs_label.markup = True
             self.tracker_hinted_locs_label.markup = True
             tracker_header.add_widget(self.tracker_total_locs_label)
