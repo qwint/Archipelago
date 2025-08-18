05f76e2d551f14ef6fc83b524b55052015d01025
qwint <qwint.42@gmail.com>
2025-08-18 00:29:38 -0500

    small refactor on new tests

--- worlds/hk_rework/test/test_resource_state_vars.py
+++ worlds/hk_rework/test/test_resource_state_vars.py
@@ -14,6 +14,41 @@ class inputs(NamedTuple):
     cs: dict[str, int] = {}
     assert_empty: bool = False
     prep_vars: Iterable[str] = ()
+    expecteds: Iterable[Iterable[tuple[int, int, int, int]]] = ()
+
+
+class StateVarSetup:
+    key: str
+    resource: dict[str, int]
+    cs: dict[str, int]
+    assert_empty: bool
+    prep_vars: Iterable[str]
+
+    def setUp(self):
+        super().setUp()
+        self.key = self.vars.key
+        self.resource = self.vars.resource
+        self.cs = self.vars.cs
+        self.assert_empty = self.vars.assert_empty
+        self.prep_vars = self.vars.prep_vars
+
+    def get_modify_args(self) -> tuple[Counter, CollectionState, int]:
+        state = CollectionState(self.multiworld)
+        for item, i in self.cs.items():
+            # assert item in self.world.item_name_to_id, f"Unknown item collected {item}"
+            for _ in range(i):
+                state.collect(self.world.create_item(item))
+        return Counter(self.resource), state, self.player
+
+    def get_modified_state(self):
+        args = self.get_modify_args()
+        if self.prep_vars:
+            for prep in self.prep_vars:
+                prep_resources = next(ResourceStateHandler.get_handler(prep).modify_state(*args))
+            args = prep_resources, args[1], args[2]  # TODO: there's some _replace thing i can look into too
+
+        handler = ResourceStateHandler.get_handler(self.key)
+        return [s for s in handler.modify_state(*args)]
 
 
 ers = {"NOPASSEDCHARMEQUIP": 0, "NOFLOWER": 0}  # Empty Resource State
@@ -32,12 +67,9 @@ input_matrix = [
     # TODO, add tests for equip charm and hp state
 
     inputs("$LIFEBLOOD", resource=ers, assert_empty=True),
-    inputs("$LIFEBLOOD", resource={**ers, "NOTCHES": 4}, cs={"Lifeblood_Heart": 1}),
-    inputs("$LIFEBLOOD", resource={**ers, "NOTCHES": 4}, cs={"Lifeblood_Core": 1}),
-    inputs("$LIFEBLOOD", resource={**ers, "NOTCHES": 4}, cs={"Joni's_Blessing": 1}),
+    *[inputs("$LIFEBLOOD", resource={**ers, "NOTCHES": 4}, cs={charm: 1}) for charm in ("Lifeblood_Heart", "Lifeblood_Core", "Joni's_Blessing")],
     inputs("$LIFEBLOOD", resource=ers, cs={"Lifeblood_Heart": 1}, assert_empty=True, prep_vars=("$TAKEDAMAGE[2]",)),
-    inputs("$LIFEBLOOD", resource=ers, cs={"Lifeblood_Core": 1}, prep_vars=("$TAKEDAMAGE[2]",)),
-    inputs("$LIFEBLOOD", resource=ers, cs={"Joni's_Blessing": 1}, prep_vars=("$TAKEDAMAGE[2]",)),
+    *[inputs("$LIFEBLOOD", resource=ers, cs={charm: 1}, prep_vars=("$TAKEDAMAGE[2]",)) for charm in ("Lifeblood_Core", "Joni's_Blessing")],
 
     # inputs("$SHADESKIP", assert_empty=True),  # theoretically checking if the option is disabled but idk
     inputs("$SHADESKIP", resource={"USEDSHADE": 1}, assert_empty=True),
@@ -55,27 +87,12 @@ input_matrix = [
 
 
 @classvar_matrix(vars=input_matrix)
-class TestStateVars(NoStepHK):
+class TestStateVars(StateVarSetup, NoStepHK):
     vars: inputs
 
-    def get_modify_args(self) -> tuple[Counter, CollectionState, int]:
-        state = CollectionState(self.multiworld)
-        for item, i in self.vars.cs.items():
-            # assert item in self.world.item_name_to_id, f"Unknown item collected {item}"
-            for _ in range(i):
-                state.collect(self.world.create_item(item))
-        return Counter(self.vars.resource), state, self.player
-
     def test_output(self):
-        args = self.get_modify_args()
-        if self.vars.prep_vars:
-            for prep in self.vars.prep_vars:
-                prep_resources = next(ResourceStateHandler.get_handler(prep).modify_state(*args))
-            args = prep_resources, args[1], args[2]  # there's some _replace thing i can look into too
-
-        handler = ResourceStateHandler.get_handler(self.vars.key)
-        outputs = [s for s in handler.modify_state(*args)]
-        if self.vars.assert_empty:
+        outputs = self.get_modified_state()
+        if self.assert_empty:
             self.assertFalse(outputs, "Expected to not be in logic but was")
         else:
             self.assertTrue(outputs, "Expected to be in logic but was not")
