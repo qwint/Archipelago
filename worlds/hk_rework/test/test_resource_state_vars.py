5a5f45c9c8623236ef86d2ef8074972e064e5bfb
qwint <qwint.42@gmail.com>
2025-08-18 09:10:38 -0500

    teehee whoops

--- worlds/hk_rework/test/test_resource_state_vars.py
+++ worlds/hk_rework/test/test_resource_state_vars.py
@@ -42,14 +42,14 @@ class StateVarSetup:
         return Counter(self.resource), state, self.player
 
     def get_modified_state(self):
-        args = self.get_modify_args()
+        rs, cs, pi = self.get_modify_args()
         if self.prep_vars:
             for prep in self.prep_vars:
                 rs = next(ResourceStateHandler.get_handler(prep).modify_state(rs, cs, pi))
             args = rs, cs, pi
 
         handler = ResourceStateHandler.get_handler(self.key)
-        return [s for s in handler.modify_state(*args)]
+        return [s for s in handler.modify_state(rs, cs, pi)]
 
 
 ers = {"NOPASSEDCHARMEQUIP": 0, "NOFLOWER": 0}  # Empty Resource State
@@ -57,31 +57,31 @@ ers = {"NOPASSEDCHARMEQUIP": 0, "NOFLOWER": 0}  # Empty Resource State
 
 input_matrix = [
     inputs("FOO=0"),
-    # inputs("$SHADESKIP", cs={"King_Fragment": 1, "Queen_Fragment": 1}, assert_empty=True),
+    inputs("$SHADESKIP", cs={"King_Fragment": 1, "Queen_Fragment": 1}, assert_empty=True),
 
-    # inputs("$CASTSPELL[3]"),
-    # inputs("$CASTSPELL[3]", prep_vars=("$SHADESKIP",), assert_empty=True),
-    # inputs("$CASTSPELL[4]", cs={"Vessel_Fragment": 3}, assert_empty=True),
-    # inputs("$CASTSPELL[4]", resource={"NOPASSEDCHARMEQUIP": 0, "NOTCHES": 6}, cs={"Spell_Twister": 1}),
-    # inputs("$CASTSPELL[3,1]", cs={"Vessel_Fragment": 3}),
+    inputs("$CASTSPELL[3]"),
+    inputs("$CASTSPELL[3]", prep_vars=("$SHADESKIP",), assert_empty=True),
+    inputs("$CASTSPELL[4]", cs={"Vessel_Fragment": 3}, assert_empty=True),
+    inputs("$CASTSPELL[4]", resource={"NOPASSEDCHARMEQUIP": 0, "NOTCHES": 6}, cs={"Spell_Twister": 1}),
+    inputs("$CASTSPELL[3,1]", cs={"Vessel_Fragment": 3}),
 
-    # # TODO, add tests for equip charm and hp state
+    # TODO, add tests for equip charm and hp state
 
-    # inputs("$LIFEBLOOD", resource=ers, assert_empty=True),
-    # *[inputs("$LIFEBLOOD", resource={**ers, "NOTCHES": 4}, cs={charm: 1}) for charm in ("Lifeblood_Heart", "Lifeblood_Core", "Joni's_Blessing")],
-    # inputs("$LIFEBLOOD", resource=ers, cs={"Lifeblood_Heart": 1}, assert_empty=True, prep_vars=("$TAKEDAMAGE[2]",)),
-    # *[inputs("$LIFEBLOOD", resource=ers, cs={charm: 1}, prep_vars=("$TAKEDAMAGE[2]",)) for charm in ("Lifeblood_Core", "Joni's_Blessing")],
+    inputs("$LIFEBLOOD", resource=ers, assert_empty=True),
+    *[inputs("$LIFEBLOOD", resource={**ers, "NOTCHES": 4}, cs={charm: 1}) for charm in ("Lifeblood_Heart", "Lifeblood_Core", "Joni's_Blessing")],
+    inputs("$LIFEBLOOD", resource=ers, cs={"Lifeblood_Heart": 1}, assert_empty=True, prep_vars=("$TAKEDAMAGE[2]",)),
+    *[inputs("$LIFEBLOOD", resource=ers, cs={charm: 1}, prep_vars=("$TAKEDAMAGE[2]",)) for charm in ("Lifeblood_Core", "Joni's_Blessing")],
 
-    # # inputs("$SHADESKIP", assert_empty=True),  # theoretically checking if the option is disabled but idk
-    # inputs("$SHADESKIP", resource={"USEDSHADE": 1}, assert_empty=True),
-    # inputs("$SHADESKIP", resource={"CHARM36": 1}, assert_empty=True),
-    # inputs("$SHADESKIP", resource={"REQUIREDMAXSOUL": 67}, assert_empty=True),
-    # inputs("$SHADESKIP"),
-    # inputs("$SHADESKIP[2HITS]", resource={"MASKSHARDS": 4}, assert_empty=True),  # TODO make sure that this aligns with how i set up damage state var in the future
-    # inputs("$SHADESKIP[2HITS]", resource={"MASKSHARDS": 16}),
-    # inputs("$SHADESKIP[2HITS]", resource={"MASKSHARDS": 8, "NOTCHES": 6}, cs={"Can_Repair_Fragile_Charms": 1, "Fragile_Heart": 1}),
-    # inputs("$SHADESKIP[2HITS]", resource={"MASKSHARDS": 8, "NOTCHES": 6}, cs={"Unbreakable_Strength": 1, "Fragile_Heart": 1}),
-    # inputs("$SHADESKIP[2HITS]", resource={"MASKSHARDS": 8, "NOTCHES": 6, "BROKEHEART": 1}, cs={"Can_Repair_Fragile_Charms": 1, "Fragile_Heart": 1}, assert_empty=True),
+    # inputs("$SHADESKIP", assert_empty=True),  # theoretically checking if the option is disabled but idk
+    inputs("$SHADESKIP", resource={"USEDSHADE": 1}, assert_empty=True),
+    inputs("$SHADESKIP", resource={"CHARM36": 1}, assert_empty=True),
+    inputs("$SHADESKIP", resource={"REQUIREDMAXSOUL": 67}, assert_empty=True),
+    inputs("$SHADESKIP"),
+    inputs("$SHADESKIP[2HITS]", resource={"MASKSHARDS": 4}, assert_empty=True),  # TODO make sure that this aligns with how i set up damage state var in the future
+    inputs("$SHADESKIP[2HITS]", resource={"MASKSHARDS": 16}),
+    inputs("$SHADESKIP[2HITS]", resource={"MASKSHARDS": 8, "NOTCHES": 6}, cs={"Can_Repair_Fragile_Charms": 1, "Fragile_Heart": 1}),
+    inputs("$SHADESKIP[2HITS]", resource={"MASKSHARDS": 8, "NOTCHES": 6}, cs={"Unbreakable_Strength": 1, "Fragile_Heart": 1}),
+    inputs("$SHADESKIP[2HITS]", resource={"MASKSHARDS": 8, "NOTCHES": 6, "BROKEHEART": 1}, cs={"Can_Repair_Fragile_Charms": 1, "Fragile_Heart": 1}, assert_empty=True),
 
     # TODO, add shrogo, slopeball, soulstate, takedamage tests
 ]
