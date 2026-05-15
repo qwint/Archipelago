1f0442b79e6b00fd00e240d72e2450efe9545fb0
qwint <qwint.42@gmail.com>
2026-05-15 15:09:30 -0500

    add v6 back so i don't have to change the workflow more

--- clear_worlds.py
+++ clear_worlds.py
@@ -4,6 +4,6 @@ import shutil
 if __name__ == "__main__":
     with os.scandir("worlds") as it:
         for entry in it:
-            if not entry.is_file() and entry.name not in ("generic", "_bizhawk"):
+            if not entry.is_file() and entry.name not in ("generic", "_bizhawk", "v6"):
                 # print(entry.path)
                 shutil.rmtree(entry.path)
