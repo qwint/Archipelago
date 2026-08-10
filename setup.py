8f45b49aec63bb05400622e20566ace2529563f3
qwint <qwint.42@gmail.com>
2026-08-09 23:17:47 -0500

    force pymem too,,

--- setup.py
+++ setup.py
@@ -645,7 +645,7 @@ cx_Freeze.setup(
     ext_modules=cythonize("_speedups.pyx"),
     options={
         "build_exe": {
-            "packages": ["worlds", "kivy", "cymem", "websockets", "kivymd", "orjson"],
+            "packages": ["worlds", "kivy", "cymem", "websockets", "kivymd", "orjson", "pymem"],
             "includes": ["rule_builder.cached_world"],
             "excludes": ["numpy", "Cython", "PySide2", "PIL",
                          "pandas"],
